7f978ca2e4e556dd238baf1f333e585ed93dba1b
huy do <huy.do2@sotatel.com>
2023-11-01 07:16:34 +0700

    feat: update favorite brand component2

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import BaseHomePage from 'src/components/layouts/BaseHomePage';
 import ProductList from './ProductList';
-import FavoriteBrand from './FavoriteBrand';
 
 const HomePage = () => {
   return (
     <>
       <BaseHomePage>
-        <FavoriteBrand />
         <ProductList />
       </BaseHomePage>
     </>
