93b19656493c67cb3ddeb813d4a4566c6dfd5b27
huy do <huy.do2@sotatel.com>
2023-12-19 07:38:42 +0700

    fix: menu sidebar

--- src/components/layouts/HomePage/MenuFilter.tsx
+++ src/components/layouts/HomePage/MenuFilter.tsx
@@ -1,101 +1,23 @@
-import { Flex, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import {
+  Flex,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from '@chakra-ui/react';
 import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
-import { useNavigate } from 'react-router-dom';
 import rf from 'src/api/RequestFactory';
 import { useState } from 'react';
 import { useEffectUnsafe } from 'src/hooks/useEffectUnsafe';
 import ProductTyByCategory from 'src/pages/Admin/ManageCategoryListPage/ProductTyByCategory.part';
 
-const MOCK_MENU = [
-  {
-    titleLabel: 'Thực phẩm chức năng',
-    icon: true,
-    dataDrop: [
-      {
-        title: 'test1',
-        link: <ProductTyByCategory categoriesID={1} />,
-      },
-      {
-        title: 'test2',
-        link: '/admin',
-      },
-      {
-        title: 'Tắt cả',
-        link: '/category-functional-foods',
-      },
-    ],
-  },
-  {
-    titleLabel: 'Thiết bị y tế',
-    icon: true,
-    dataDrop: [
-      {
-        title: 'test1',
-        link: '/',
-      },
-      {
-        title: 'test2',
-        link: '/',
-      },
-      {
-        title: 'Tất cả',
-        link: '/',
-      },
-    ],
-  },
-  {
-    titleLabel: 'Chăm sóc cá nhân',
-    icon: true,
-    dataDrop: [
-      {
-        title: 'test1',
-        link: '/',
-      },
-      {
-        title: 'test2',
-        link: '/',
-      },
-      {
-        title: 'Tất cả',
-        link: '/',
-      },
-    ],
-  },
-  {
-    titleLabel: 'Thuốc',
-    icon: true,
-    dataDrop: [
-      {
-        title: 'test1',
-        link: '/',
-      },
-      {
-        title: 'test2',
-        link: '/',
-      },
-      {
-        title: 'Tất cả',
-        link: '/category-medicine',
-      },
-    ],
-  },
-  {
-    titleLabel: 'Tìm kiếm địa chỉ',
-    link: '/pharmacy-system',
-    icon: false,
-  },
-  {
-    titleLabel: 'Thông tin nhà thuốc',
-    icon: false,
-    link: 'https://pharmacy-documentation.vercel.app/docs/intro',
-  },
-];
-
 const MenuFilter = () => {
-  const navigate = useNavigate();
-
   const [cateList, setCateList] = useState<any>([]);
-  const [typeList, setTypeList] = useState<any>([]);
 
   const getAllDrugsTypeByCateID = async (id: number | string) => {
     try {
@@ -105,6 +27,7 @@ const MenuFilter = () => {
       const newTypeList = resType.map((item: any) => ({
         id: item.id,
         name: item.name,
+        content: <ProductTyByCategory categoriesID={1} />,
       }));
       return newTypeList;
     } catch (e: any) {
@@ -130,15 +53,16 @@ const MenuFilter = () => {
     }
   };
 
-  const tabs = cateList.map((item: any) => ({
-    id: item.id,
-    name: item.name,
-    // content: <CategoryFunctionalFoods categoriesID={item.id} />,
-  }));
+  console.log(cateList);
+
+  // const tabs = cateList.map((item: any) => ({
+  //   id: item.id,
+  //   name: item.name,
+  //   // content: <CategoryFunctionalFoods categoriesID={item.id} />,
+  // }));
 
   useEffectUnsafe(() => {
     getAllCate();
-    // getAllDrugsTypeByCateID;
   }, []);
 
   return (
@@ -152,7 +76,7 @@ const MenuFilter = () => {
         {cateList.map((menu: any, index: any) => {
           return (
             <Flex key={index}>
-              <Menu>
+              {/* <Menu>
                 {({ isOpen }) => (
                   <>
                     <MenuButton
@@ -165,14 +89,6 @@ const MenuFilter = () => {
                         borderBottom: '1px solid #1250dc',
                         transition: 'border-bottom 0.3s ease',
                       }}
-                      // onClick={() => {
-                      //   if (menu.link && menu.link.startsWith('http')) {
-                      //     window.open(menu.link, '_blank');
-                      //   } else if (menu.link) {
-                      //     navigate(menu.link);
-                      //   }
-                      // }}
-                      onClick={() => getAllDrugsTypeByCateID(menu.id)}
                       cursor="pointer"
                     >
                       <Flex alignItems={'center'} gap={'3px'}>
@@ -204,7 +120,57 @@ const MenuFilter = () => {
                     )}
                   </>
                 )}
-              </Menu>
+              </Menu> */}
+              {/* <Tabs
+                h={'full'}
+                display="flex"
+                flexDirection={'column'}
+                variant={'unstyled'}
+                colorScheme="transparent"
+                // defaultIndex={defaultTab}
+                className="app-tab"
+                isLazy
+                // overflow={overflow}
+              >
+                <TabList className="tab-list">
+                  <Flex
+                    justifyContent={'space-between'}
+                    alignItems="center"
+                    w="100%"
+                  >
+                    <Flex>
+                      {tabs.map((tab: ITabs) => {
+                        return (
+                          <Tab
+                            id={tab.id}
+                            key={tab.id}
+                            className="app-tab__name-tab"
+                            onClick={() => onChange && onChange(tab.id)}
+                          >
+                            {tab.name}
+                          </Tab>
+                        );
+                      })}
+                    </Flex>
+
+                    <Box>{rightElement ? rightElement() : ''}</Box>
+                  </Flex>
+                </TabList>
+
+                <TabPanels flex={1}>
+                  {tabs.map((tab: any) => {
+                    return (
+                      <TabPanel
+                        key={tab.id}
+                        h={'full'}
+                        className="app-tab__content-tab"
+                      >
+                        {tab.content}
+                      </TabPanel>
+                    );
+                  })}
+                </TabPanels>
+              </Tabs> */}
             </Flex>
           );
         })}
